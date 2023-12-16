1cd559d3351215569aed4a3666d6e534f290a0cd
Lukas Andreano <business.nikitap@gmail.com>
2023-12-16 09:59:10 +0600

    fix: changed domain

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,5 +1,5 @@
 export default async function fetch2(method, params) {
     let fetch_params = params || ''
-    const data = await fetch(`https://monitoring.nbalin.dev/` + method + `?` + fetch_params + `&` + window.location.href.slice(window.location.href.indexOf('?') + 1))
+    const data = await fetch(`https://monitoring-app.koka.team/` + method + `?` + fetch_params + `&` + window.location.href.slice(window.location.href.indexOf('?') + 1))
     return await data.json()
 }
